ce7a6221d254f8f2d3ae110f5d7cf57a20bb90fd
Anatolii <fiver73@mail.ru>
2022-09-06 21:40:02 +0400

    реализовал цикл для тестирования ввода хода, формирования и вывода игрового поля

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 """Модуль верхнего уровня для учебного проекта."""
+
+from func_io import print_playing_field, merge_turn_field, input_turn
+
+field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+player = 0
+# field = [[0, 5, 1],[0, 2, 3],[0, 0, 4]]
+
+
+while True:
+    player = (player + 1) % 2
+    print_playing_field(field, player)
+    turn = input_turn(player)
+    if turn == '':
+        break
+    merge_turn_field(int(turn), field)
