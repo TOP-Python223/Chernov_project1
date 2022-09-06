820074d56da7595849544f2d3223e49d721781d4
Anatolii <fiver73@mail.ru>
2022-09-06 21:48:24 +0400

    добавил описание глобальных переменных

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 from func_io import print_playing_field, merge_turn_field, input_turn
 
+# структура игрового поля
 field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# номер текущего игрока (0 - играющий Х, 1 - играющий О)
 player = 0
 # field = [[0, 5, 1],[0, 2, 3],[0, 0, 4]]
 
