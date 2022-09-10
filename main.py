384382dab1a9894cf144cce1243e1ffab9c209d8
Gennadiy <renardes@gmail.com>
2022-09-10 19:22:14 +0500

    правки импортов

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Модуль верхнего уровня для учебного проекта."""
 
-from func_io import *
+import func_io
 # import func_io
 # print_playing_field, merge_turn_field, input_turn
+
 
 # структура игрового поля
 field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -12,15 +13,16 @@ players = {'curr_player': 0, 'player': [{'name': '', 'age': 0}, {'name': '','age
 players['player'][0] = {'name': 'Паша', 'age': 11}
 players['player'][1] = {'name': 'Сева', 'age': 14}
 
+
 while True:
     # отображаем игровое поле
-    print_playing_field(field, players)
+    func_io.print_playing_field(field, players)
 
-    # запрашиваем ход или командbpу игрока
-    turn = input_turn(players)
+    # запрашиваем ход или команду игрока
+    turn = func_io.input_turn(players)
     # выход при пустом вводе (пока)
     if turn == '':
         break
     # заносим ход во внутреннее представление
-    merge_turn_field(int(turn), field)
+    func_io.merge_turn_field(int(turn), field)
     players['curr_player'] = (players['curr_player'] + 1) % 2
