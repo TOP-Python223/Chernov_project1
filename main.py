613f079ba91a8ecc78229d577b9594f644054e92
Anatolii <fiver73@mail.ru>
2022-09-10 06:49:54 +0400

    Расширил структуру игрока в архитектуре и реализации структуры

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ from func_io import *
 # структура игрового поля
 field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 # номер текущего игрока (0 - играющий Х, 1 - играющий О)
-players = {'curr_player': 0, 'player': [{'name': ''}, {'name': ''}]}
+players = {'curr_player': 0, 'player': [{'name': '', 'age': 0}, {'name': '','age': 0}]}
 # field = [[0, 5, 1],[0, 2, 3],[0, 0, 4]]
-
+players['player'][0] = {'name': 'Паша', 'age': 11}
+players['player'][1] = {'name': 'Сева', 'age': 14}
 
 while True:
     # отображаем игровое поле
     print_playing_field(field, players)
 
-    players['player'][0]['name'] = 'Игрок1'
-    players['player'][1]['name'] = 'Игрок за O'
     # запрашиваем ход или командbpу игрока
     turn = input_turn(players)
     # выход при пустом вводе (пока)
