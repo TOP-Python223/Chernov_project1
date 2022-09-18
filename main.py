23d1869a7d3b9255bb6326bc790a66b6e4b06689
Anatolii <fiver73@mail.ru>
2022-09-18 16:06:39 +0400

    исправил косяк с определением выигрыша по вертикали, реализация не оптимальна.

--- main.py
+++ main.py
@@ -6,39 +6,43 @@ import game
 
 
 # структура игрового поля
-field = [[0, 0, 0],
+g_field = [[0, 0, 0],
          [0, 0, 0],
          [0, 0, 0]]
 # номер текущего игрока (0 - играющий Х, 1 - играющий О)
-players = {'curr_player': 0,
+g_players = {'curr_player': 0,
            'player': [
                {'name': '', 'age': 0},
                {'name': '','age': 0}
             ]
            }
+
 # field = [[0, 5, 1],[0, 2, 3],[0, 0, 4]]
-players['player'][0] = {'name': 'Паша', 'age': 11}
-players['player'][1] = {'name': 'Сева', 'age': 14}
+g_players['player'][0] = {'name': 'Паша', 'age': 11}
+g_players['player'][1] = {'name': 'Сева', 'age': 14}
 
 
 
-field = [[0, 2, 4],
-         [1, 3, 0],
-         [0, 0, 0]]
+# field = [[0, 2, 4],
+#          [1, 3, 0],
+#          [0, 0, 0]]
 while True:
     # отображаем игровое поле
-    str_field = game.show_field(field, players)
+    str_field = game.show_field(g_field, g_players)
     print(str_field)
     # запрашиваем ход или команду игрока
-    turn = game.input_turn(players)
+    turn = game.input_turn(g_players)
     # выход при пустом вводе (пока)
     if turn == '':
         break
     # заносим ход во внутреннее представление
-    game.merge_turn_field(int(turn), field)
-    if game.check_win(field, turn):
-        print('Celebration! YRW!!!')
-        str_field = game.show_field(field, players)
+    game.merge_turn_field(int(turn), g_field)
+    if game.check_win(g_field, turn):
+        # str_message = dict(players['player'][players['curr_player']]).name
+        # print(f"Celebration!" + str_message + ", YRW!!!")
+        pl_name = g_players['player'][g_players['curr_player']]['name']
+        print(f"Celebration! {pl_name}, YRW!!!")
+        str_field = game.show_field(g_field, g_players)
         print(str_field)
         break
-    players['curr_player'] = (players['curr_player'] + 1) % 2
+    g_players['curr_player'] = (g_players['curr_player'] + 1) % 2
