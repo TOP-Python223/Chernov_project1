c8109cf4b9f051e5aae38b99db2a4e58be507064
Anatolii <fiver73@mail.ru>
2022-09-28 05:20:06 +0400

    Описание внутренних функций

--- game.py
+++ game.py
@@ -3,17 +3,17 @@
 #                       Глобальные константы
 # -----------------------------------------------------------------------------
 # количество клеток игрового поля по горизонтали
-c_field_columns: int = 3
+G_FIELD_COLUMNS: int = 3
 # количество клеток игрового поля по вертикали
-c_field_rows: int = 3
+G_FIELD_ROWS: int = 3
 # размер одной ячейки по горизонтали в символах
-c_cell_width: int = 3
+G_CELL_WIDTH: int = 3
 # размер окна терминала по горизонтали в символах
-c_term_width: int = 80
+G_TERM_WIDTH: int = 80
 # размер игрового поля по горизонтали в символах
-c_field_width = c_field_columns * c_cell_width + c_field_columns - 1
+G_FIELD_WIDTH = G_FIELD_COLUMNS * G_CELL_WIDTH + G_FIELD_COLUMNS - 1
 # символы для обозначения ходов на игровом поле
-c_marks = ('X', 'O')
+G_MARKS = ('X', 'O')
 
 #                       Функции ввода-вывода
 # -----------------------------------------------------------------------------
@@ -24,9 +24,9 @@ c_marks = ('X', 'O')
 def input_turn(p_players):
     # ИСПОЛЬЗОВАТЬ: приведение к bool
     if p_players['curr_player']:
-        sign_char = c_marks[1]
+        sign_char = G_MARKS[1]
     else:
-        sign_char = c_marks[0]
+        sign_char = G_MARKS[0]
     pl_name = p_players['player'][p_players['curr_player']]['name']
     # ИСПОЛЬЗОВАТЬ: цикл для проверки пользовательского ввода
     while True:
@@ -59,7 +59,7 @@ def input_turn(p_players):
 def merge_turn_field(p_turn: int, p_field):
     """функция объединения очередного хода с матицей ходов"""
     turn_number: int = max(sum(p_field, [])) + 1
-    p_field[p_turn // c_field_columns][p_turn % c_field_rows] = turn_number
+    p_field[p_turn // G_FIELD_COLUMNS][p_turn % G_FIELD_ROWS] = turn_number
 
 
 # отображение игрового поля
@@ -70,26 +70,26 @@ def show_field(p_field, p_players) -> str:
     """функция объединения очередного хода с матицей ходов"""
     result = ''
     if p_players['curr_player']:
-        start_char = ' '*(c_term_width - c_field_width)
+        start_char = ' '*(c_term_width - G_FIELD_WIDTH)
     else:
         start_char = ''
-    for r in range(c_field_rows):
+    for r in range(G_FIELD_ROWS):
         result = result + start_char
-        for c in range(c_field_columns):
-            finish_char = '' if c == c_field_columns - 1 else '|'
+        for c in range(G_FIELD_COLUMNS):
+            finish_char = '' if c == G_FIELD_COLUMNS - 1 else '|'
             if p_field[r][c] == 0:
                 print_char = ' '
             elif p_field[r][c] % 2:
-                print_char = c_marks[0]
+                print_char = G_MARKS[0]
             else:
-                print_char = c_marks[1]
+                print_char = G_MARKS[1]
             # print(print_char, end=finish_char)
-            result = result + print_char.center(c_cell_width) + finish_char
+            result = result + print_char.center(G_CELL_WIDTH) + finish_char
         result = result + '\n'
-        if r < c_field_rows - 1:
-            # print(start_char + '-'*c_field_width)
+        if r < G_FIELD_ROWS - 1:
+            # print(start_char + '-'*G_FIELD_WIDTH)
             #
-            result = result + start_char + '—' * (c_field_width) + '\n'
+            result = result + start_char + '—' * (G_FIELD_WIDTH) + '\n'
     return result
 
 
@@ -107,27 +107,28 @@ def check_win(p_field, p_turn: int) -> bool:
     def check_row() -> bool:
         """по горионтали"""
         count = 0
-        for c in range(c_field_columns):
-            count += (p_field[p_turn // c_field_columns][c] % 2 == p_turn % 2) and (p_field[p_turn // c_field_columns][c] != 0)
-        return count == c_field_columns
+        for c in range(G_FIELD_COLUMNS):
+            count += (p_field[p_turn // G_FIELD_COLUMNS][c] % 2 == p_turn % 2) and (p_field[p_turn // G_FIELD_COLUMNS][c] != 0)
+        return count == G_FIELD_COLUMNS
 
     # Проверка текущего хода по вертикали на выигрыш
     #
     def check_column() -> bool:
         """по вертикали"""
         count = 0
-        for r in range(c_field_rows):
-            if p_field[0][p_turn % c_field_rows] != 0:
-                v_check = p_field[0][p_turn % c_field_rows]
+        for r in range(G_FIELD_ROWS):
+            if p_field[0][p_turn % G_FIELD_ROWS] != 0:
+                v_check = p_field[0][p_turn % G_FIELD_ROWS]
             else:
                 v_check = 0
-            count += (p_field[r][p_turn % c_field_rows] % 2 == v_check % 2) and (p_field[r][p_turn % c_field_rows] != 0)
-        return count == c_field_rows
+            count += (p_field[r][p_turn % G_FIELD_ROWS] % 2 == v_check % 2) and (p_field[r][p_turn % G_FIELD_ROWS] != 0)
+        return count == G_FIELD_ROWS
 
     # Проверка текущего хода по диагоналям на выигрыш
     def check_cross() -> bool:
+        """по горизонтали"""
         return False
 
-    # тело функции check_win
+    # результат функции check_win
     return check_row() or check_column() or check_cross()
 
