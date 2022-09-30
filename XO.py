c69bd60f5c0bd1d2986afdd533a4cf16391482b9
Anatolii <fiver73@mail.ru>
2022-09-30 21:17:25 +0400

    перенес глобальные константы в модуль g_consts

--- XO.py
+++ XO.py
@@ -2,19 +2,7 @@
 #                       Глобальные константы
 
 import game
-import g_cont as G
-
-# номер текущего игрока (0 - играющий Х, 1 - играющий О)
-g_players = {'curr_player': 0,
-             'player': [
-                 {'name': '', 'age': 0},
-                 {'name': '', 'age': 0}
-             ]
-             }
-
-# field = [[0, 5, 1],[0, 2, 3],[0, 0, 4]]
-g_players['player'][0] = {'name': 'Паша', 'age': 11}
-g_players['player'][1] = {'name': 'Сева', 'age': 14}
+import g_consts as G
 
 while True:
     # отображаем игровое поле
@@ -27,10 +15,8 @@ while True:
         break
     # заносим ход во внутреннее представление
     game.merge_turn_field(G.FIELD, int(turn))
+    # проверяем на победу
     if game.check_win(G.FIELD, turn):
         pl_name = g_players['player'][g_players['curr_player']]['name']
         print(f"Celebration! {pl_name}, YRW!!!")
-        str_field = game.show_field(G.FIELD, g_players)
-        print(str_field)
-        break
     g_players['curr_player'] = (g_players['curr_player'] + 1) % 2
