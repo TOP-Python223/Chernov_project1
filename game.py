8bd5013649bab2a09abe531063cbf148d6443e96
Anatolii <fiver73@mail.ru>
2022-10-01 03:07:44 +0400

    изменил функции, используя глобальные переменные

--- game.py
+++ game.py
@@ -4,18 +4,18 @@
 
 import g_consts as G
 
-def input_turn(p_players):
+def input_turn():
     """Функция запроса ввода команды или хота игрока."""
     # параметры:
     #   p_player - игроки струкруры, описанной
     # возвращает введенную пользователем строку
     # ИСПОЛЬЗОВАТЬ: приведение к bool
     v_turn = None
-    if p_players['curr_player']:
+    if G.PLAYERS['curr_player']:
         sign_char = G.MARKS[1]
     else:
         sign_char = G.MARKS[0]
-    pl_name = p_players['player'][p_players['curr_player']]['name']
+    pl_name = G.PLAYERS['player'][G.PLAYERS['curr_player']]['name']
     # ИСПОЛЬЗОВАТЬ: цикл для проверки пользовательского ввода
     while True:
         # строка, которую ввел пользователь в запросе хода
@@ -36,22 +36,21 @@ def input_turn(p_players):
         if not(v_turn is None):
             return v_turn
 
-def merge_turn_field(p_field, p_turn: int):
+def merge_turn_field(p_turn: int):
     """функция объединения очередного хода с матицей ходов"""
     # параметры:
     #   p_turn - номер выбранной клетки
-    #   p_field - игровое поле
-    turn_number: int = max(sum(p_field, [])) + 1
-    p_field[p_turn // G.FIELD_COLUMNS][p_turn % G.FIELD_COLUMNS] = turn_number
+    turn_number: int = max(sum(G.FIELD, [])) + 1
+    G.FIELD[p_turn // G.FIELD_COLUMNS][p_turn % G.FIELD_COLUMNS] = turn_number
 
-def show_field(p_field, p_players) -> str:
+def show_field() -> str:
     """функция возвращает строку игрового поля для отображения"""
     # отображение игрового поля
     #   p_field - игровое поле
     #   p_player - игрок (0 - играющий Х, 1 - играющий О)
     # возврат: строка игрового поля по домашнему заданию # HW_09.04.txt
     result = ''
-    if p_players['curr_player']:
+    if G.PLAYERS['curr_player']:
         start_char = ' ' * (G.TERM_WIDTH - G.FIELD_WIDTH)
     else:
         start_char = ''
@@ -59,9 +58,9 @@ def show_field(p_field, p_players) -> str:
         result = result + start_char
         for c in range(G.FIELD_COLUMNS):
             finish_char = '' if c == G.FIELD_COLUMNS - 1 else '|'
-            if p_field[r][c] == 0:
+            if G.FIELD[r][c] == 0:
                 print_char = ' '
-            elif p_field[r][c] % 2:
+            elif G.FIELD[r][c] % 2:
                 print_char = G.MARKS[0]
             else:
                 print_char = G.MARKS[1]
@@ -75,7 +74,7 @@ def show_field(p_field, p_players) -> str:
     return result
 
 
-def check_win(p_field, p_turn: int) -> bool:
+def check_win(p_turn: int) -> bool:
     """функция проверки выигрышного хода"""
     #    вход: объекты с данными о сделанных ходах
     #    p_field - игровое поле
@@ -87,15 +86,15 @@ def check_win(p_field, p_turn: int) -> bool:
     # номер колонки хода
     v_col_num = p_turn % G.FIELD_COLUMNS
     # 1 - ход игрока 1, 0 - ход игрока 2
-    v_odd = p_field[v_row_num][v_col_num] % 2
+    v_odd = G.FIELD[v_row_num][v_col_num] % 2
     def check_row() -> bool:
-        """по горионтали"""
+        """по горизонтали"""
         #    вход: None
         #    возврат: bool - есть ли на горизонтали победная комбинация
         # количество четных или нечетных должно быть равно G.COUNT_WIN
         v_count = 0
         # print(f'{p_turn=}  {v_odd=}')
-        for c in p_field[v_row_num]:
+        for c in G.FIELD[v_row_num]:
             # print(f'{c=}')
             v_count += (c % 2 == v_odd) and (c != 0)
         # print(f'{v_count} | {G.COUNT_WIN}')
@@ -108,7 +107,7 @@ def check_win(p_field, p_turn: int) -> bool:
         # вертикаль вычисляется как деление нацело хода на количество столбцов
         # количество четных или нечетных должно быть равно G.COUNT_WIN по условиям задания
         v_count = 0
-        for v_row in p_field:
+        for v_row in G.FIELD:
             v_count += (v_row[v_col_num] % 2 == v_odd) and (v_row[v_col_num] != 0)
         return v_count == G.COUNT_WIN
 
@@ -122,11 +121,11 @@ def check_win(p_field, p_turn: int) -> bool:
             i_row_num = v_row_num - i_loop
             i_col_num = v_col_num + i_loop
             if (i_row_num >= 0) and (i_col_num < G.FIELD_COLUMNS):
-                v_count += (p_field[i_row_num][i_col_num] % 2 == v_odd) and (p_field[i_row_num][i_col_num] != 0)
+                v_count += (G.FIELD[i_row_num][i_col_num] % 2 == v_odd) and (G.FIELD[i_row_num][i_col_num] != 0)
             i_row_num = v_row_num + i_loop
             i_col_num = v_col_num - i_loop
             if (i_row_num < G.FIELD_ROWS) and (i_col_num >= 0):
-                v_count += (p_field[i_row_num][i_col_num] % 2 == v_odd) and (p_field[i_row_num][i_col_num] != 0)
+                v_count += (G.FIELD[i_row_num][i_col_num] % 2 == v_odd) and (G.FIELD[i_row_num][i_col_num] != 0)
             i_loop += 1
             if (v_row_num - i_loop < 0) and \
                 (v_col_num - i_loop < 0) and \
@@ -144,11 +143,11 @@ def check_win(p_field, p_turn: int) -> bool:
             i_row_num = v_row_num - i_loop
             i_col_num = v_col_num - i_loop
             if (i_row_num >= 0) and (i_col_num >= 0):
-                v_count += (p_field[i_row_num][i_col_num] % 2 == v_odd) and (p_field[i_row_num][i_col_num] != 0)
+                v_count += (G.FIELD[i_row_num][i_col_num] % 2 == v_odd) and (G.FIELD[i_row_num][i_col_num] != 0)
             i_row_num = v_row_num + i_loop
             i_col_num = v_col_num + i_loop
             if (i_row_num < G.FIELD_ROWS) and (i_col_num < G.FIELD_COLUMNS):
-                v_count += (p_field[i_row_num][i_col_num] % 2 == v_odd) and (p_field[i_row_num][i_col_num] != 0)
+                v_count += (G.FIELD[i_row_num][i_col_num] % 2 == v_odd) and (G.FIELD[i_row_num][i_col_num] != 0)
 
             i_loop += 1
             if (v_row_num - i_loop < 0) and \
